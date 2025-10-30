9ca842127801a15805c10ee52f326f6e2d422b1e
dejanakop <dkop@griddynamics.com>
2025-10-30 13:08:08 +0100

    Create GitHub Runner Network

---
+++ github-actions-runner/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = "1.13.2"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.0.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "dkop-capstone-github-runner-tf-backend"
+  }
+}
+
+provider "google" {
+  project = var.project
+  region = var.region
+  zone = var.zone
+}
+
+resource "google_project_service" "compute_api" {
+  project = var.project
+  service = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_compute_network" "runner_vpc" {
+  name = "${var.base_name}-vpc"
+  auto_create_subnetworks = var.auto_create_subnetworks
+  depends_on = [ google_project_service.compute_api ]
+}
+
+resource "google_compute_subnetwork" "runner_subnet" {
+  name = "${var.base_name}-subnet"
+  network = google_compute_network.runner_vpc.id
+  ip_cidr_range = var.ip_cidr_range
+}
+
+resource "google_compute_firewall" "runner_firewall" {
+  name = "${var.base_name}-firewall"
+  network = google_compute_network.runner_vpc.id
+  allow {
+    protocol = var.protocol
+    ports = var.ports
+  }
+  source_ranges = var.source_ranges
+  target_tags = var.target_tags
+}
+
---
+++ github-actions-runner/variables.tf
@@ -0,0 +1,15 @@
+# BASE NAME
+variable "base_name" { type = string }
+
+# PROVIDER
+variable "project" { type = string }
+variable "region" { type = string }
+variable "zone" { type = string }
+
+# NETWORK
+variable "auto_create_subnetworks" { type = bool }
+variable "ip_cidr_range" { type = string }
+variable "protocol" { type = string }
+variable "ports" { type = list(string) }
+variable "source_ranges" { type = list(string) }
+variable "target_tags" { type = list(string) }
