606b1a57458da2a1a5a9e9b4bcfa307a951d51eb
dejanakop <dkop@griddynamics.com>
2025-10-30 15:23:03 +0100

    Download GitHub Runner on VM

--- github-actions-runner/variables.tf
+++ github-actions-runner/variables.tf
@@ -17,3 +17,4 @@ variable "target_tags" { type = list(string) }
 # VM
 variable "runner_machine_type" { type = string }
 variable "runner_image" { type = string }
+variable "ssh_key_path" { type = string }
