bacf205bc7653fc0faddd2e289dd480190d42825
dejanakop <dkop@griddynamics.com>
2025-11-10 09:38:19 +0100

    Remove Helm Module

--- main/terraform/modules/helm/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "release_name" { type = string }
-variable "repository" { type = string }
-variable "chart_name" { type = string }
-variable "namespace" { type = string }
-variable "values" { type = list(string) }
