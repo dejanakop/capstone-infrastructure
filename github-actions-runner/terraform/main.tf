961cf0e1624183425467b4f7a063f884229bad01
dejanakop <dkop@griddynamics.com>
2025-11-13 09:06:32 +0100

    Update main.tf

--- github-actions-runner/terraform/main.tf
+++ github-actions-runner/terraform/main.tf
@@ -92,8 +92,8 @@ resource "google_compute_instance" "runner_vm" {
   #!/bin/bash
   
   # INSTALLING DOCKER
-  sudo apt update -y
-  sudo apt install -y ca-certificates curl gnupg
+  sudo apt-get update -y
+  sudo apt-get install -y ca-certificates curl gnupg
   sudo install -y -m 0755 -d /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -101,8 +101,8 @@ resource "google_compute_instance" "runner_vm" {
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt update -y
-  sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get update -y
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   sudo usermod -aG docker $USER
   
   # Wait for network
