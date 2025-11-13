bded2115e24182873020dcf844071f4594ca3385
dejanakop <dkop@griddynamics.com>
2025-11-13 09:12:42 +0100

    Update main.tf

--- github-actions-runner/terraform/main.tf
+++ github-actions-runner/terraform/main.tf
@@ -93,16 +93,13 @@ resource "google_compute_instance" "runner_vm" {
   
   # INSTALLING DOCKER
   sudo apt-get update -y
-  sudo apt-get install -y ca-certificates curl gnupg
-  sudo install -y -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  sudo chmod a+r /etc/apt/keyrings/docker.gpg
-  echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get install -y curl jq tar unzip apt-transport-https ca-certificates gnupg lsb-release software-properties-common
+  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update -y
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+  sudo systemctl enable docker
+  sudo systemctl start docker
   sudo usermod -aG docker $USER
   
   # Wait for network
