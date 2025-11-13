e1d559c91e11194b15b3fc56077c5ee9cef9f971
dejanakop <dkop@griddynamics.com>
2025-11-13 08:54:06 +0100

    Update main.tf

--- github-actions-runner/terraform/main.tf
+++ github-actions-runner/terraform/main.tf
@@ -90,12 +90,20 @@ resource "google_compute_instance" "runner_vm" {
 
   metadata_startup_script = <<-EOF
   #!/bin/bash
-  set -euo pipefail
   
-  LOGFILE=/var/log/github-runner-startup.log
-  exec > >(tee -a $LOGFILE) 2>&1
-  
-  echo "===== STARTUP SCRIPT START ====="
+  # INSTALLING DOCKER
+  sudo apt update -y
+  sudo apt install -y ca-certificates curl gnupg
+  sudo install -y -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt update -y
+  sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo usermod -aG docker $USER
   
   # Wait for network
   echo "Waiting for network..."
